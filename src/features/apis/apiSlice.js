a40aba363b9b5c93459804ab3bb26bb0d7735294
Rahat92 <61516015+Rahat92@users.noreply.github.com>
2025-01-20 15:23:27 +0600

    Update apiSlice.js base url

--- src/features/apis/apiSlice.js
+++ src/features/apis/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const apiSlice = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: process.env.REACT_APP_API_URL,
+    baseUrl: `https://meal-management-05ry.onrender.com`,
   }),
   tagTypes: ["getMeals", "getAllMonthStat", 'getMessages', 'createMessage'],
   endpoints: (builder) => ({}),
