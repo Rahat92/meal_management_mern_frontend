d7b0eee973a089e6937db539ce302dadce127b59
Rahat92 <khrahat92@gmail.com>
2025-06-20 16:57:18 +0600

    change api

--- src/pages/MealSheets.js
+++ src/pages/MealSheets.js
@@ -8,7 +8,7 @@ import AddSheetModal from "../components/AddSheetModal";
 import { Button } from "../components/TailwindStyledComponent/Button";
 const MealSheets = () => {
   const { data: yearMonths } = useGetYearMonthQuery();
-  const [deleteYearMonth, {isSuccess}] = useDeleteYearMonthMutation()
+  const [deleteYearMonth, { isSuccess }] = useDeleteYearMonthMutation()
   const dispatch = useDispatch();
   const [showModal, setShowModal] = React.useState(false);
   useEffect(() => {
@@ -16,7 +16,7 @@ const MealSheets = () => {
   }, []);
 
   useEffect(() => {
-    if(isSuccess){
+    if (isSuccess) {
       alert('Successfully Delete A month!')
     }
   }, [isSuccess])
@@ -34,7 +34,7 @@ const MealSheets = () => {
           <thead className="bg-green-500 font-bold">
             <tr>
               <td>Month</td>
-              <td>Action</td>
+              <td className="w-full text-center">Action</td>
             </tr>
           </thead>
           {yearMonths?.yearMonth?.map((yearMonth) => (
@@ -67,17 +67,19 @@ const MealSheets = () => {
                                         : ""}{" "}
                 {yearMonth?.year}
               </td>
-              <td>
+              <td className="text-center w-full">
                 <button
                   onClick={() => {
                     const isConfirm = window.confirm('Are you sure you want to delete a month completely?')
-                    if(isConfirm){
+                    if (isConfirm) {
                       deleteYearMonth(yearMonth)
                     }
                   }}
                   className={style.btn}
                 >
-                  Delete
+                  <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="25" height="25" viewBox="0 0 48 48">
+                    <path d="M 20.5 4 A 1.50015 1.50015 0 0 0 19.066406 6 L 14.640625 6 C 12.796625 6 11.086453 6.9162188 10.064453 8.4492188 L 7.6972656 12 L 7.5 12 A 1.50015 1.50015 0 1 0 7.5 15 L 40.5 15 A 1.50015 1.50015 0 1 0 40.5 12 L 40.302734 12 L 37.935547 8.4492188 C 36.913547 6.9162187 35.202375 6 33.359375 6 L 28.933594 6 A 1.50015 1.50015 0 0 0 27.5 4 L 20.5 4 z M 8.9726562 18 L 11.125 38.085938 C 11.425 40.887937 13.77575 43 16.59375 43 L 31.40625 43 C 34.22325 43 36.574 40.887938 36.875 38.085938 L 39.027344 18 L 8.9726562 18 z"></path>
+                  </svg>
                 </button>
               </td>
             </tr>
