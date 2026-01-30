4ab38c93709242c6aa4ad437c3c7308668ac4403
Rahat92 <khrahat92@gmail.com>
2026-01-31 05:54:18 +0600

    update backend api

--- src/features/apis/apiSlice.js
+++ src/features/apis/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const apiSlice = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: 'http://localhost:5000/api/v1',
+    baseUrl: process.env.REACT_APP_API_URL,
   }),
   tagTypes: ["getMeals", "getAllMonthStat", 'getMessages', 'createMessage'],
   endpoints: (builder) => ({}),
